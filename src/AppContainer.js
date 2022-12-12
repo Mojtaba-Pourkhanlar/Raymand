9148bbae201901eb3b1500e9b5b06161051a9669
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2022-12-12 10:52:36 +0330

    custom theme

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,17 +1,35 @@
-import UserID from "frontEnd/components/user";
-import Users from "frontEnd/components/users";
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+// mui RTL
+import { CacheProvider } from "@emotion/react";
+import rtlPlugin from "stylis-plugin-rtl";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+// MUI Theme
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "frontEnd/mui/theme";
+// Components
+import UserID from "frontEnd/components/user";
+import Users from "frontEnd/components/users";
 
 const AppContainer = () => {
+  //NOTE Create RTL Cache
+  const cacheRTL = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+
   return (
-    <>
-      <Routes>
-        <Route path="/users/:id" element={<UserID />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/*" element={<Navigate to="/users" />} />
-      </Routes>
-    </>
+    <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/users/:id" element={<UserID />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/*" element={<Navigate to="/users" />} />
+        </Routes>
+      </ThemeProvider>
+    </CacheProvider>
   );
 };
 
