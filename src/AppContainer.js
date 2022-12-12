44cd52d607b3d5946bd8892080d524f8b023b8aa
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2022-12-12 10:43:05 +0330

    create directory

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import UserID from "frontEnd/components/user";
+import Users from "frontEnd/components/users";
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const AppContainer = () => {
   return (
-    <div>AppContainer</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/users/:id" element={<UserID />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/*" element={<Navigate to="/users" />} />
+      </Routes>
+    </>
+  );
+};
 
-export default AppContainer
+export default AppContainer;
